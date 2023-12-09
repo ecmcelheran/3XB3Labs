1d7e193b828400e8c9a4ac7bace3a7df0e162d07
LunaAljammal <122303107+LunaAljammal@users.noreply.github.com>
2023-12-08 23:12:58 -0500

    Update refactored.py

--- Shortest_Path/refactored.py
+++ Shortest_Path/refactored.py
@@ -53,9 +53,67 @@ class Bellman_Ford(SPAlgorithm):
                         pred[neighbour] = node
         return dist[dest]
 
-class A_Star_Adapter(SPAlgorithm):
+
+class Astar:
+    def a_star(G, s, d, h):
+    pred = {}
+    dist = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    #Initialize priority queue/heap and distances
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(s, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+        if current_node == d:
+            return
+        for neighbour in G.adj[current_node]:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour) + h[neighbour])
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
 
 
+    # Creating graph and heuristic for experiment suite 2
+    def tube_map():
+        g = DirectedWeightedGraph()
+        with open('london_stations.csv', 'r') as node:
+            csvreader = csv.reader(node)
+            next(csvreader)
+            for row in csvreader:
+                g.add_node_coord(int(row[0]), float(row[1]), float(row[2]))
+    
+        with open('london_connections.csv', 'r') as edge:
+            csvEdgeReader = csv.reader(edge)
+            next(csvEdgeReader)
+            for row in csvEdgeReader:
+                g.add_edge_line(int(row[0]), int(row[1]), int(row[3]), int(row[2]))
+        return g
+    
+    
+    def heuristic_function(g, t):
+        h = {t: 0}
+        x1 = g.coord[t][0]
+        y1 = g.coord[t][1]
+        for node in g.adj:
+            x2 = g.coord[node][0]
+            y2 = g.coord[node][1]
+            h[node] = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        return h
+class Adapter(SPAlgorithm):
+    def __init__(self, G):
+        self.class_Astar = Astar()
+        self.G = G  # Assuming G is an instance of DirectedWeightedGraph
+
+    def find_shortest_path(self, start, destination):
+        h = heuristic_function(self.G, destination)
+        self.class_Astar.a_star(self.G, start, destination, h)
 
 class ShortPathFinder:
     def __init__(self, graph: Graph, algorithm: SPAlgorithm):
